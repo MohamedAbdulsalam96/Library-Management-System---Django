89316408c9f369b2002f2b97a700fab5075d2c0e
Mohamed_Abdulsalam <moha2016it@gmail.com>
2022-02-26 02:23:19 +0300

    Tables &linking &dynamic pages

--- libraryMS/library/views.py
+++ libraryMS/library/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
     context = {
-        'books': B
+        'cate': Category.objects.all(),
+        'books': Book.objects.all(),
     }
-    return render(request,'pages/index.html', context)
+    return render(request,'pages/index.html', context )
+
+
+def books(request):
+    context = {
+        'cate': Category.objects.all(),
+        'books': Book.objects.all(),
+    }
+    return render(request,'pages/books.html', context)
--- libraryMS/libraryMS/settings.py
+++ libraryMS/libraryMS/settings.py
@@ -123,3 +123,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'libraryMS/static')]
 
+# media files 
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, '[libraryMS/static ')]
